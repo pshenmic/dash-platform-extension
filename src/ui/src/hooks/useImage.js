a6317b701d7242aa75c4b7eff0f5104f2093302e
Alexey <andreewsky.alexey@gmail.com>
2025-05-05 23:24:13 +0300

    Fix useImage hook: add return to hook

--- src/ui/src/hooks/useImage.js
+++ src/ui/src/hooks/useImage.js
@@ -1,6 +1,6 @@
 export const useImage = (imageName) => {
-  if (chrome.extension) {
-    chrome.extension.getURL(imageName)
+  if (chrome.runtime) {
+    return chrome.runtime.getURL(imageName)
   } else {
     return `assets/${imageName}`
   }
