0742ac9493a392b20ebd7408222c00c29ff93d9d
Alexey <andreewsky.alexey@gmail.com>
2025-05-06 21:42:27 +0300

    Fix useImage condition

--- src/ui/src/hooks/useImage.js
+++ src/ui/src/hooks/useImage.js
@@ -1,5 +1,5 @@
 export const useImage = (imageName) => {
-  if (chrome.runtime) {
+  if (chrome?.runtime?.getURL) {
     return chrome.runtime.getURL(imageName)
   } else {
     return `assets/${imageName}`
