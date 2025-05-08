5d48bf74ac49bd0cf894f815fdf2d9bb35236b4d
Alexey <andreewsky.alexey@gmail.com>
2025-05-08 16:17:34 +0300

    Update Test.tsx file with cva variants example

--- src/ui/src/states/home/Test.tsx
+++ src/ui/src/states/home/Test.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import { cva } from 'class-variance-authority'
+
+const testClasses = cva('btn-base', {
+  variants: {
+    state: {
+      active: 'bg-green-500',
+      disabled: 'bg-gray-500',
+    },
+  },
+  defaultVariants: { state: 'active' },
+});
 
 export default function () {
-    return (<div>Test</div>)
+  const isDisabled = true
+
+  return (
+    <div className={testClasses({ state: isDisabled ? 'disabled' : 'active' })}>
+      Test
+      <div className={'TestClass'}>TestClass</div>
+    </div>
+  )
 }
