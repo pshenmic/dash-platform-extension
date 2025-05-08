ee96673cb9539e22aa7c4fcc1370005a4ba6aa98
Alexey <andreewsky.alexey@gmail.com>
2025-05-08 10:34:25 +0300

    Implement tailwind in project and add condition to mode in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const mode = process.env.NODE_ENV
 
 module.exports = {
   entry: {
     'ui': './src/ui/index.js',
-    // 'content-script': './src/content-script.js',
-    // 'injected': './src/injected.js',
+    ...(mode === 'production' && {
+      'content-script': './src/content-script.js',
+      'injected': './src/injected.js',
+    })
   },
   output: {
     publicPath: '',
@@ -18,7 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
       { test: /\.tsx?$/, loader: "ts-loader" },
