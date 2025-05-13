d43fbb50433fe391d99066845fe3cfc96f589874
Alexey <andreewsky.alexey@gmail.com>
2025-05-13 16:56:06 +0300

    Update back button styles in header

--- src/ui/components/layout/header/index.tsx
+++ src/ui/components/layout/header/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { cva } from 'class-variance-authority'
 import { useNavigate, useMatches } from 'react-router-dom'
 import { useStaticAsset } from '../../../../hooks/useStaticAsset'
+import { Button } from '../../controls/buttons'
 
 const IMAGE_VARIANTS = {
   coins: {
     src: 'coin_bagel.png',
     alt: 'Badge',
-    positionClasses: 'max-w -mt-[22%]'
+    imgClasses: 'max-w -mt-[22%]',
+    containerClasses: 'w-[120%] -mr-[55%]'
   },
 } as const
 
@@ -43,10 +45,6 @@ const headerStyles = cva(
   'relative flex items-start justify-between'
 )
 
-const backButtonStyles = cva(
-  'p-2 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 hover:bg-gray-100'
-)
-
 export default function Header () {
   const matches = useMatches() as Match[]
   const navigate = useNavigate()
@@ -77,39 +75,23 @@ export default function Header () {
       </div>
 
       {right && (
-        <div className={'w-[120%] -mr-[55%]'}>
-          {right.variant === 'image'
-            ? (() => {
-              const { src, alt, positionClasses } = IMAGE_VARIANTS[right.imageType]
-              return (
+        right.variant === 'image'
+          ? (() => {
+            const { src, alt, imgClasses, containerClasses } = IMAGE_VARIANTS[right.imageType]
+            return (
+              <div className={containerClasses}>
                 <img
                   src={useStaticAsset(src)}
                   alt={alt}
-                  className={`relative ${positionClasses} max-w-[348px] max-h-[327px]`}
-                />
-              )
-            })()
-            : <button
-              onClick={handleBack}
-              className={backButtonStyles()}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M15 19l-7-7 7-7'
-                />
-              </svg>
-            </button>
-          }
-        </div>
+                  className={`relative ${imgClasses} max-w-[348px] max-h-[327px]`}
+              />
+              </div>
+            )
+          })()
+          : <Button onClick={handleBack}>
+            {'<'}
+            Back
+          </Button>
       )}
     </header>
   )
