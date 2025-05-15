4c3936a90f47a5ebfce5e3f6f046584f1003422f
Alexey <andreewsky.alexey@gmail.com>
2025-05-15 23:41:39 +0300

    Update HomeState styles

--- src/ui/states/home/HomeState.tsx
+++ src/ui/states/home/HomeState.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import './home.state.css'
 import NoIdentities from './NoIdentities'
 import { useIdentitiesStore } from '../../../stores/identitiesStore'
+import { Button } from '../../components/controls/buttons'
+import ValueCard from '../../components/containers/ValueCard'
+import Text from '../../text/Text'
+import BigNumber from '../../components/data/BigNumber'
+import { NotActive } from '../../components/data/NotActive'
+import './home.state.css'
+import Identifier from "../../components/data/Indetifier";
 
 export default function () {
   const currentIdentity = useIdentitiesStore((state) => state.currentIdentity)
@@ -35,35 +41,67 @@ export default function () {
       .catch(() => setTransactionsLoadError(true))
   }, [])
 
+  console.log('transactions', transactions)
+
   return (
-    <div className={'HomeState__Container'}>
-      <select className={'HomeState__Identity__Select'}>
-        {identities.map((identity) => <option
-          key={identity.identifier}
-          value={identity.identifier}>
-          {identity.identifier}
-        </option>)}
-      </select>
-      <span className={'HomeState__Identity__Balance'}>Balance: {identity.balance}</span>
+    <div className={'flex flex-col gap-4'}>
+      <ValueCard colorScheme={'lightBlue'}>
+        <div className={'flex flex-col gap-1'}>
+          <select className={'HomeState__Identity__Select'}>
+            {identities.map((identity) => <option
+              key={identity.identifier}
+              value={identity.identifier}>
+              {identity.identifier}
+            </option>)}
+          </select>
 
-      <div className={'HomeState__Identity__Buttons'}>
-        <div className={'HomeState__Identity__Buttons__Send'}>Send</div>
-        <div className={'HomeState__Identity__Buttons__Withdraw'}>Withdraw</div>
+          <div className={'flex flex-col gap-[0.125rem]'}>
+            <Text dim>Balance</Text>
+            <span>
+              {!Number.isNaN(Number(identity?.balance))
+                ? <Text size={'xl'} weight={'bold'} monospace>
+                  <BigNumber>
+                    {identity.balance}
+                  </BigNumber>
+                </Text>
+                : <NotActive>N/A</NotActive>
+              }
+              <Text
+                size={'lg'}
+                className={'ml-2'}
+              >
+                Credits
+              </Text>
+            </span>
+          </div>
+        </div>
+      </ValueCard>
 
+      <div className={'flex gap-2'}>
+        <Button className={'w-1/2'} disabled>Send</Button>
+        <Button color={'gray'} variant={'outline'} className={'w-1/2'} disabled>Withdraw</Button>
       </div>
 
-      <div className={'HomeState__Transactions'}>
-        <span className={'HomeState__Transactions__Title'}>Transactions:</span>
+      <div>
+        <Text size={'lg'} weight={'bold'}>Transactions:</Text>
 
         {transactionsLoadError &&
-          <div className={'HomeState__Transactions__Error'}>Error during loading transactions, please try again
-            later</div>}
+          <div>
+            Error during loading transactions, please try again later
+          </div>
+        }
 
         <div className={'HomeState__Transactions__Container'}>
-          {transactions?.length && transactions.map((transaction) => <div key={transaction.hash}
-                                                                          className={'HomeState__Transactions__Item'}>
-            <a target={'_blank'}
-               href={`https://testnet.platform-explorer.com/transaction/${transaction.hash}`}>{transaction.hash}</a>
+          {transactions?.length && transactions.map((transaction) =>
+            <div key={transaction.hash} className={'HomeState__Transactions__Item'}>
+              <a
+                target={'_blank'}
+                href={`https://testnet.platform-explorer.com/transaction/${transaction.hash}`}
+              >
+                <ValueCard clickable>
+                  <Identifier highlight={'both'} ellipsis>{transaction.hash}</Identifier>
+                </ValueCard>
+              </a>
           </div>)}
         </div>
       </div>
