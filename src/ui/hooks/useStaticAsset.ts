da9118b677d6dc06ae391440c5a54eb3c378e2db
Alexey <andreewsky.alexey@gmail.com>
2025-06-19 15:57:09 +0300

    Add types to useStaticAsset

--- src/ui/hooks/useStaticAsset.ts
+++ src/ui/hooks/useStaticAsset.ts
@@ -1,5 +1,5 @@
-export const useStaticAsset = (assetName) => {
-  if (chrome?.runtime?.getURL) {
+export const useStaticAsset = (assetName: string): string => {
+  if (chrome?.runtime?.getURL != null) {
     return chrome.runtime.getURL(`assets/${assetName}`)
   } else {
     return `assets/${assetName}`
