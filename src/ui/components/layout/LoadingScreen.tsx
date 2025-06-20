0f574d878f9f82aad508cba27b25d17460c8970d
Alexey <andreewsky.alexey@gmail.com>
2025-06-20 16:54:19 +0300

    Create LoadingScreen

---
+++ src/ui/components/layout/LoadingScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Text from '../../text/Text'
+
+interface LoadingScreenProps {
+  message?: string
+}
+
+export default function LoadingScreen ({ message = 'Loading...' }: LoadingScreenProps): React.JSX.Element {
+  return (
+    <div className='flex flex-col gap-4 items-center justify-center min-h-[200px]'>
+      <div className='animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600' />
+      <Text color='blue' size='lg'>
+        {message}
+      </Text>
+    </div>
+  )
+}
