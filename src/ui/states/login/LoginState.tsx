4e3f81f470775c63542356f58396ce9005b52e2e
Alexey <andreewsky.alexey@gmail.com>
2025-07-31 17:55:10 +0300

    Update login state styles

--- src/ui/states/login/LoginState.tsx
+++ src/ui/states/login/LoginState.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button, Text, Input } from 'dash-ui/react'
+import { Button, Text, Input, Heading, DashLogo } from 'dash-ui/react'
 import { useExtensionAPI } from '../../hooks/useExtensionAPI'
 
 export default function LoginState (): React.JSX.Element {
@@ -44,40 +44,51 @@ export default function LoginState (): React.JSX.Element {
   }
 
   return (
-    <div className='flex flex-col gap-4'>
-      <Text size='xl' weight='bold'>
-        Enter Password
-      </Text>
+    <div className='flex flex-col'>
+      <div className='flex items-center flex-col w-full gap-2.5 mb-6'>
+        <DashLogo containerSize='3rem'/>
 
-      <Text color='blue'>
-        Enter your password to unlock the wallet
-      </Text>
+        <Heading level={1} size='2xl'>
+          Welcome Back
+        </Heading>
 
-      <div className='flex flex-col gap-2'>
-        <Input
-          type='password'
-          placeholder='Enter password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          size='xl'
-          colorScheme='default'
-        />
+        <Text dim className='leading-tight' size='sm'>
+          Use the password to unlock your wallet.
+        </Text>
       </div>
 
-      {error != null && (
-        <div className='text-red-500 text-sm'>
-          {error}
+      <div className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-2 w-full'>
+          <Text dim>
+            Password
+          </Text>
+          <Input
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            size='xl'
+            colorScheme='default'
+            className='w-full'
+          />
         </div>
-      )}
 
-      <Button
-        colorScheme='brand'
-        onClick={handleLoginClick}
-        disabled={password === '' || isLoading}
-        className='w-full'
-      >
-        {isLoading ? 'Logging in...' : 'Login'}
-      </Button>
+        {error != null && (
+          <div className='text-red-500 text-sm'>
+            {error}
+          </div>
+        )}
+
+        <Button
+          size='xl'
+          colorScheme='brand'
+          onClick={handleLoginClick}
+          disabled={password === '' || isLoading}
+          className='w-full'
+        >
+          {isLoading ? 'Logging in...' : 'Unlock'}
+        </Button>
+      </div>
     </div>
   )
 }
