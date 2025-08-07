9cba1c31ca8967ef230d99cef32af298a0b67a80
Alexey <andreewsky.alexey@gmail.com>
2025-08-07 19:52:48 +0300

    Add types and exports for menu

---
+++ src/ui/components/settings/index.ts
@@ -0,0 +1,17 @@
+export { SettingsMenu } from './SettingsMenu'
+export { SettingsMenuButton } from './SettingsMenuButton'
+export { OverlayMenu } from './OverlayMenu'
+export { MenuItem } from './MenuItem'
+export { MenuSection } from './MenuSection'
+
+// Screens
+export { MainSettingsScreen } from './screens/MainSettingsScreen'
+export { WalletSettingsScreen } from './screens/WalletSettingsScreen'
+export { SecuritySettingsScreen } from './screens/SecuritySettingsScreen'
+export { PreferencesScreen } from './screens/PreferencesScreen'
+export { ConnectedDappsScreen } from './screens/ConnectedDappsScreen'
+export { HelpSupportScreen } from './screens/HelpSupportScreen'
+export { AboutScreen } from './screens/AboutScreen'
+
+// Types
+export type { MenuItem as MenuItemType, MenuSection as MenuSectionType, SettingsScreenProps } from './types'
