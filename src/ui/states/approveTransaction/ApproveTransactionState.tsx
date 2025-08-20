5f95ccee4182d4b9b040d9d92640496b74791920
Alexey <andreewsky.alexey@gmail.com>
2025-08-20 16:59:47 +0300

    Replace redirect link in ApproveTransactionState

--- src/ui/states/approveTransaction/ApproveTransactionState.tsx
+++ src/ui/states/approveTransaction/ApproveTransactionState.tsx
@@ -225,7 +225,7 @@ function ApproveTransactionState (): React.JSX.Element {
 
         <div className='flex flex-col gap-2 w-full'>
           <Button
-            onClick={async () => await navigate('/import-keystore')} // Todo: redirect link
+            onClick={async () => await navigate('/choose-wallet-import-type')}
             colorScheme='brand'
           >
             Import Identity
