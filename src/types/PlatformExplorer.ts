16fa4a927ea84480be45210e9892c37da2fec6fc
Alexey <andreewsky.alexey@gmail.com>
2025-08-20 21:48:11 +0300

    Update PlatformExplorer types

--- src/types/PlatformExplorer.ts
+++ src/types/PlatformExplorer.ts
@@ -50,3 +50,69 @@ export interface TransactionsResponse {
   resultSet: TransactionData[]
   error?: string | null
 }
+
+export interface TokenOwner {
+  identifier: string
+  aliases?: Array<{
+    alias: string
+    contested: boolean
+    documentId: string
+    status: string
+    timestamp: string
+  }> | null
+}
+
+export interface TokenData {
+  identifier: string
+  position: number
+  timestamp: string | null
+  description: string
+  localizations: {
+    [key: string]: {
+      pluralForm: string
+      singularForm: string
+      shouldCapitalize: boolean
+    }
+  }
+  baseSupply: string
+  totalSupply: string
+  maxSupply: string | null
+  owner: TokenOwner
+  mintable: boolean
+  burnable: boolean
+  freezable: boolean
+  unfreezable: boolean
+  destroyable: boolean
+  allowedEmergencyActions: boolean
+  dataContractIdentifier: string
+  changeMaxSupply: boolean
+  totalGasUsed: number | null
+  mainGroup: string | null
+  totalTransitionsCount: number | null
+  totalFreezeTransitionsCount: number | null
+  totalBurnTransitionsCount: number | null
+  decimals: number
+  perpetualDistribution: {
+    type: string
+    recipientType: string
+    recipientValue: string | null
+    interval: number
+    functionName: string
+    functionValue: {
+      amount: string
+    }
+  } | null
+  preProgrammedDistribution: any | null
+}
+
+export interface TokensPagination {
+  page: number
+  limit: number
+  total: number
+}
+
+export interface TokensResponse {
+  resultSet: TokenData[]
+  pagination: TokensPagination
+  error?: string | null
+}
