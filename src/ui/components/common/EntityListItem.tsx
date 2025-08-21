b67269f507e0cc997c1e38c7b7ff77badca2066b
Alexey <andreewsky.alexey@gmail.com>
2025-08-21 13:45:25 +0300

    Create entity list items

---
+++ src/ui/components/common/EntityListItem.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+interface EntityListItemProps {
+  children: React.ReactNode
+  href?: string
+  onClick?: () => void
+  className?: string
+}
+
+function EntityListItem({
+  children,
+  href,
+  onClick,
+  className = ''
+}: EntityListItemProps): React.JSX.Element {
+  const baseClasses = `entities-list-item ${className}`
+
+  if (href) {
+    return (
+      <a
+        href={href}
+        target='_blank'
+        rel='noopener noreferrer'
+        className={baseClasses}
+      >
+        {children}
+      </a>
+    )
+  }
+
+  if (onClick) {
+    return (
+      <div
+        className={baseClasses}
+        onClick={onClick}
+        role='button'
+        tabIndex={0}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            onClick()
+          }
+        }}
+      >
+        {children}
+      </div>
+    )
+  }
+
+  return (
+    <div className={baseClasses}>
+      {children}
+    </div>
+  )
+}
+
+export default EntityListItem
