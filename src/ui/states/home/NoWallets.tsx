b441ce4d5ce2b6f5d21b8550ba61ef5342c73477
Alexey <andreewsky.alexey@gmail.com>
2025-08-27 21:51:40 +0300

    Update NoWallets and NoIdentities blocks with EmptyState

--- src/ui/states/home/NoWallets.tsx
+++ src/ui/states/home/NoWallets.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button } from 'dash-ui/react'
-// import './home.state.css'
+import { EmptyState } from '../../components/layout/EmptyState'
+import { Text } from 'dash-ui/react'
 
 export default function NoWallets (): React.JSX.Element {
   const navigate = useNavigate()
@@ -12,17 +12,11 @@ export default function NoWallets (): React.JSX.Element {
 
   return (
     <div className='screen-content'>
-      <div className='HomeState__NoIdentities__Container'>
-        <h1 className='h1-title'>
-          <b>You have<br />no wallets in this network</b>
-        </h1>
-
-        <div className='HomeState__NoIdentities__Buttons'>
-          <Button onClick={handleCreateClick}>
-            Create Wallet
-          </Button>
-        </div>
-      </div>
+      <EmptyState
+        title={<>You <Text weight='bold' color='blue' className='!text-[size:inherit] !leading-[inherit]'>Don't Have any wallets</Text> in this network</>}
+        buttonText="Add a wallet"
+        onButtonClick={handleCreateClick}
+      />
     </div>
   )
 }
