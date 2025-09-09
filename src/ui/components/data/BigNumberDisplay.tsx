4e617f2afde750e8669dfb68651adb8466d94b09
Alexey <andreewsky.alexey@gmail.com>
2025-09-09 18:12:21 +0300

    Create BigNumberDisplay component

---
+++ src/ui/components/data/BigNumberDisplay.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BigNumber, Text } from 'dash-ui-kit/react'
+import currencyRound, { isTooBigNumber } from '../../../utils'
+
+interface BigNumberDisplayProps {
+  children: number | string | bigint
+  className?: string
+}
+
+function BigNumberDisplay({ children, className = '' }: BigNumberDisplayProps): React.JSX.Element {
+  const number = children
+
+  return isTooBigNumber(number)
+  ? <Text monospace={true} className={className ?? ''}>{currencyRound(number)}</Text>
+  : <BigNumber className={className ?? ''}>{Number(number)}</BigNumber>
+}
+
+export default BigNumberDisplay
