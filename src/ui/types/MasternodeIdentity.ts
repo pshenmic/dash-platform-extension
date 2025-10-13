e62b22568e3b565b745fcaa8d1cb098e5d9b56f9
Alexey <andreewsky.alexey@gmail.com>
2025-10-13 23:35:12 +0300

    Add types to MasternodeIdentity.ts

--- src/ui/types/MasternodeIdentity.ts
+++ src/ui/types/MasternodeIdentity.ts
@@ -4,3 +4,41 @@ export interface MasternodeIdentityInput {
   votingKey: string
   payoutKey: string
 }
+
+export interface PublicKeyData {
+  keyId: number
+  purpose: string
+  securityLevel: string
+  type: string
+  isAvailable?: boolean
+}
+
+export interface IdentityPublicKey {
+  keyId: number
+  purpose?: number
+  securityLevel?: number
+  keyType?: number
+  getPublicKeyHash: () => string
+}
+
+export interface IdentityPreviewData {
+  id: string
+  name?: string
+  balance: string
+  publicKeys: PublicKeyData[]
+}
+
+export interface MasternodeIdentityPreviewData {
+  identities: Array<{
+    id: string
+    type: 'masternode' | 'voting'
+    balance: string
+    publicKeys: PublicKeyData[]
+  }>
+  keys: {
+    ownerHex: string
+    votingHex?: string
+    payoutHex?: string
+  }
+  proTxHash: string
+}
