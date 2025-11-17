5340aa131ddac97d946ad96a3ad0072981d9383f
Alexey <andreewsky.alexey@gmail.com>
2025-11-17 20:35:37 +0300

    Titles styles in TransactionSummaryCard

--- src/ui/components/cards/TransactionSummaryCard.tsx
+++ src/ui/components/cards/TransactionSummaryCard.tsx
@@ -22,7 +22,7 @@ export function TransactionSummaryCard ({
     <div className='flex flex-col gap-3 p-3 bg-white rounded-[0.9375rem] shadow-[0px_0px_35px_0px_rgba(0,0,0,0.1)]'>
       {/* Fees Row */}
       <div className='flex items-center justify-between w-full'>
-        <Text size='xs' weight='medium' className='text-dash-primary-dark-blue opacity-50'>
+        <Text size='xs' weight='medium' className='text-dash-primary-dark-blue opacity-50' dim>
           Fees:
         </Text>
         <Text size='xs' weight='medium' className='text-dash-primary-dark-blue opacity-50 text-right'>
@@ -33,7 +33,7 @@ export function TransactionSummaryCard ({
       {/* Will be sent Row - Only for Credits */}
       {showWillBeSent && selectedAsset === 'credits' && willBeSent != null && (
         <div className='flex items-center justify-between w-full'>
-          <Text size='xs' weight='medium' className='text-dash-primary-dark-blue opacity-50'>
+          <Text size='xs' weight='medium' className='text-dash-primary-dark-blue opacity-50' dim>
             Will be sent:
           </Text>
           <Text size='xs' weight='medium' className='text-dash-primary-dark-blue opacity-50 text-right'>
