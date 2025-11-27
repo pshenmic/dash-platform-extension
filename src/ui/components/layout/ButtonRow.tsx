999bc03f61eaec36c07e08d0df1ec22c2aa5a850
Alexey <andreewsky.alexey@gmail.com>
2025-11-27 18:52:40 +0300

    Create ButtonRow component

---
+++ src/ui/components/layout/ButtonRow.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Button } from 'dash-ui-kit/react'
+
+interface ButtonConfig {
+  text: string
+  onClick: () => void
+  colorScheme?: 'brand' | 'mint' | 'gray' | 'red' | 'lightBlue' | 'lightGray'
+  disabled?: boolean
+}
+
+interface ButtonRowProps {
+  leftButton: ButtonConfig
+  rightButton: ButtonConfig
+  className?: string
+}
+
+export default function ButtonRow ({ leftButton, rightButton, className }: ButtonRowProps): React.JSX.Element {
+  const handleLeftClick = leftButton.onClick
+  const handleRightClick = rightButton.onClick
+
+  return (
+    <div className={`flex gap-2 w-full ${className ?? ''}`}>
+      <Button
+        onClick={handleLeftClick}
+        colorScheme={leftButton.colorScheme}
+        className='w-1/2'
+        disabled={leftButton.disabled ?? false}
+      >
+        {leftButton.text}
+      </Button>
+      <Button
+        onClick={handleRightClick}
+        colorScheme={rightButton.colorScheme}
+        className='w-1/2'
+        disabled={rightButton.disabled ?? false}
+      >
+        {rightButton.text}
+      </Button>
+    </div>
+  )
+}
