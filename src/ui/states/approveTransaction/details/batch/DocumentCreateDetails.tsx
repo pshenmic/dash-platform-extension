bd604b1bc3008e64230ce9dbc0d7276425b4d887
Alexey <andreewsky.alexey@gmail.com>
2025-12-15 18:26:33 +0300

    Update DocumentCreateDetails

--- src/ui/states/approveTransaction/details/batch/DocumentCreateDetails.tsx
+++ src/ui/states/approveTransaction/details/batch/DocumentCreateDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, Identifier } from 'dash-ui-kit/react'
 import { TransactionDetailsCard } from '../../../../components/transactions'
+import { BatchActions, type BatchActionCode } from '../../../../../enums/BatchActions'
 
 interface DocumentCreateDetailsProps {
   transition: any
@@ -9,102 +10,65 @@ interface DocumentCreateDetailsProps {
 export function DocumentCreateDetails ({ transition }: DocumentCreateDetailsProps): React.JSX.Element {
   return (
     <>
-      {transition.id != null && (
-        <TransactionDetailsCard title='Document ID'>
-          <Identifier className='!text-[1.25rem]' avatar copyButton middleEllipsis edgeChars={5}>
-            {transition.id}
-          </Identifier>
-        </TransactionDetailsCard>
-      )}
-
-      {transition.type != null && (
-        <TransactionDetailsCard title='Document Type'>
-          <Text size='lg' weight='medium'>{transition.type}</Text>
-        </TransactionDetailsCard>
-      )}
-
+      {/* Data Contract Identifier */}
       {transition.dataContractId != null && (
-        <TransactionDetailsCard title='Data Contract ID'>
+        <TransactionDetailsCard title='Data Contract Identifier'>
           <Identifier className='!text-[1.25rem]' avatar copyButton middleEllipsis edgeChars={5}>
             {transition.dataContractId}
           </Identifier>
         </TransactionDetailsCard>
       )}
 
+      {/* Document Type + Action */}
+      {(transition.type != null || transition.action != null) && (
+        <div className='flex gap-2.5'>
+          {transition.type != null && (
+            <TransactionDetailsCard title='Document Type' className='flex-1'>
+              <Text size='sm' weight='medium'>{transition.type}</Text>
+            </TransactionDetailsCard>
+          )}
+          {transition.action != null && (
+            <TransactionDetailsCard title='Action' className='flex-1'>
+              <Text size='sm' weight='medium'>
+                {BatchActions[transition.action as BatchActionCode]?.title ?? transition.action}
+              </Text>
+            </TransactionDetailsCard>
+          )}
+        </div>
+      )}
+
+      {/* Document Identifier */}
+      {transition.id != null && (
+        <TransactionDetailsCard title='Document Identifier'>
+          <Identifier className='!text-[1.25rem]' avatar copyButton middleEllipsis edgeChars={5}>
+            {transition.id}
+          </Identifier>
+        </TransactionDetailsCard>
+      )}
+
+      {/* Revision + Identity Contract Nonce */}
+      {(transition.revision != null || transition.identityContractNonce != null) && (
+        <div className='flex gap-2.5'>
+          {transition.revision != null && (
+            <TransactionDetailsCard title='Revision'>
+              <Text size='sm' weight='medium'>{transition.revision}</Text>
+            </TransactionDetailsCard>
+          )}
+          {transition.identityContractNonce != null && (
+            <TransactionDetailsCard title='Identity Contract Nonce' className='flex-1'>
+              <Text size='sm' weight='medium'>{transition.identityContractNonce}</Text>
+            </TransactionDetailsCard>
+          )}
+        </div>
+      )}
+
+      {/* Data */}
       {transition.data != null && (
-        <TransactionDetailsCard title='Document Data'>
-          <pre className='text-xs overflow-auto break-all whitespace-pre-wrap max-h-[200px]'>
-            {JSON.stringify(transition.data, null, 2)}
-          </pre>
-        </TransactionDetailsCard>
-      )}
-
-      {transition.entropy != null && (
-        <TransactionDetailsCard title='Entropy'>
-          <Text size='sm' className='font-mono break-all'>{transition.entropy}</Text>
-        </TransactionDetailsCard>
-      )}
-
-      {transition.revision != null && (
-        <TransactionDetailsCard title='Revision'>
-          <Text size='lg'>{transition.revision}</Text>
-        </TransactionDetailsCard>
-      )}
-
-      {transition.identityContractNonce != null && (
-        <TransactionDetailsCard title='Identity Contract Nonce'>
-          <Text size='lg'>{transition.identityContractNonce}</Text>
-        </TransactionDetailsCard>
-      )}
-
-      {transition.prefundedVotingBalance != null && (
-        <TransactionDetailsCard title='Prefunded Voting Balance'>
-          <div className='flex flex-col gap-1.5'>
-            {Object.entries(transition.prefundedVotingBalance).map(([key, value]) => (
-              <div key={key} className='flex justify-between'>
-                <Text size='sm' color='muted'>{key}:</Text>
-                <Text size='sm' className='font-medium'>{String(value)}</Text>
-              </div>
-            ))}
-          </div>
-        </TransactionDetailsCard>
-      )}
-
-      {transition.tokenPaymentInfo != null && (
-        <TransactionDetailsCard title='Token Payment Info'>
-          <div className='flex flex-col gap-2'>
-            {transition.tokenPaymentInfo.paymentTokenContractId != null && (
-              <div className='flex flex-col gap-1'>
-                <Text size='sm' color='muted'>Payment Token Contract:</Text>
-                <Identifier className='!text-[0.875rem]' copyButton middleEllipsis edgeChars={4}>
-                  {transition.tokenPaymentInfo.paymentTokenContractId}
-                </Identifier>
-              </div>
-            )}
-            {transition.tokenPaymentInfo.tokenContractPosition != null && (
-              <div className='flex justify-between'>
-                <Text size='sm' color='muted'>Token Position:</Text>
-                <Text size='sm'>{transition.tokenPaymentInfo.tokenContractPosition}</Text>
-              </div>
-            )}
-            {transition.tokenPaymentInfo.minimumTokenCost != null && (
-              <div className='flex justify-between'>
-                <Text size='sm' color='muted'>Min Cost:</Text>
-                <Text size='sm' className='font-medium'>{transition.tokenPaymentInfo.minimumTokenCost}</Text>
-              </div>
-            )}
-            {transition.tokenPaymentInfo.maximumTokenCost != null && (
-              <div className='flex justify-between'>
-                <Text size='sm' color='muted'>Max Cost:</Text>
-                <Text size='sm' className='font-medium'>{transition.tokenPaymentInfo.maximumTokenCost}</Text>
-              </div>
-            )}
-            {transition.tokenPaymentInfo.gasFeesPaidBy != null && (
-              <div className='flex justify-between'>
-                <Text size='sm' color='muted'>Gas Fees Paid By:</Text>
-                <Text size='sm'>{transition.tokenPaymentInfo.gasFeesPaidBy}</Text>
-              </div>
-            )}
+        <TransactionDetailsCard title='Data'>
+          <div className='relative max-h-[200px] overflow-auto'>
+            <pre className='text-sm font-medium opacity-75 whitespace-pre-wrap break-all'>
+              {JSON.stringify(transition.data, null, 2)}
+            </pre>
           </div>
         </TransactionDetailsCard>
       )}
