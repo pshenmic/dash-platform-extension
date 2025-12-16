2dea10ac9df6526002d7b1cb3faec8c543816648
Alexey <andreewsky.alexey@gmail.com>
2025-12-16 17:52:17 +0300

    Update TransactionDetailsCard paddings

--- src/ui/components/transactions/TransactionDetailsCard.tsx
+++ src/ui/components/transactions/TransactionDetailsCard.tsx
@@ -9,7 +9,7 @@ interface TransactionDetailsCardProps {
 
 export function TransactionDetailsCard ({ title, children, className }: TransactionDetailsCardProps): React.JSX.Element {
   return (
-    <ValueCard colorScheme='white' size='xl' border={false} className={`dash-shadow-lg ${className ?? ''}`}>
+    <ValueCard colorScheme='white' size='xl' border={false} className={`dash-shadow-lg !p-4 ${className ?? ''}`}>
       <div className='flex flex-col gap-2.5 w-full'>
         <Text size='md' dim>{title}</Text>
         {children}
