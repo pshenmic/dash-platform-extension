126f417f35267466ecfe8a15d100bb413fa601d2
Alexey <andreewsky.alexey@gmail.com>
2026-01-14 18:19:26 +0300

    Disable Create Key button

--- src/ui/components/settings/screens/PrivateKeysScreen.tsx
+++ src/ui/components/settings/screens/PrivateKeysScreen.tsx
@@ -622,6 +622,7 @@ export const PrivateKeysScreen: React.FC<SettingsScreenProps> = ({ currentIdenti
               onClick={handleCreateKey}
               colorScheme='lightBlue'
               className='flex-1'
+              disabled
             >
               Create New Key
             </Button>
