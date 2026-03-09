478df24145854d19c82713d19020ec3264d8342d
Alexey <andreewsky.alexey@gmail.com>
2026-03-09 20:58:24 +0300

    Update hex display in SigningErrorDetails

--- src/ui/components/errors/SigningErrorDetails.tsx
+++ src/ui/components/errors/SigningErrorDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Text, ValueCard, Identifier } from 'dash-ui-kit/react'
+import { Text, ValueCard } from 'dash-ui-kit/react'
 import { FieldLabel } from '../typography'
+import { CopyButton } from 'dash-ui-kit/react'
 
 export interface SigningErrorDetailsProps {
   name: string
@@ -14,16 +15,14 @@ export function SigningErrorDetails ({ name, message, hex }: SigningErrorDetails
       <div className='flex flex-col gap-2'>
         <FieldLabel>{name}</FieldLabel>
         <ValueCard colorScheme='yellow' size='xl' border={false} className='flex flex-col gap-2'>
-          <Text size='sm'>{message}</Text>
+          <Text size='sm' className='whitespace-pre-wrap break-words max-w-full'>{message}</Text>
         </ValueCard>
       </div>
       {hex != null && (
         <div className='flex flex-col gap-2'>
-          <FieldLabel>Transaction Hex</FieldLabel>
-          <ValueCard colorScheme='lightGray' size='xl' border={false} className='flex flex-col gap-2'>
-            <Identifier copyButton linesAdjustment={false}>
-              {hex}
-            </Identifier>
+          <ValueCard colorScheme='lightGray' size='xl' border={false} className='flex gap-2 justify-center'>
+            <Text size='sm' className='whitespace-pre-wrap break-words'>Copy Transaction Hex</Text>
+            <CopyButton text={hex} />
           </ValueCard>
         </div>
       )}
