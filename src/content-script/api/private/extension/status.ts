93323523ecd57982777bee655c096f6eb051ca24
AlexeyTripleA <andreewsky.alexey@gmail.com>
2026-06-14 22:25:50 +0300

    Add hasAnyWallet parameter to status

--- src/content-script/api/private/extension/status.ts
+++ src/content-script/api/private/extension/status.ts
@@ -1,14 +1,17 @@
 import { APIHandler } from '../../APIHandler'
 import { GetStatusResponse } from '../../../../types/messages/response/GetStatusResponse'
 import { StorageAdapter } from '../../../storage/storageAdapter'
+import { WalletRepository } from '../../../repository/WalletRepository'
 import { EmptyPayload } from '../../../../types/messages/payloads/EmptyPayload'
 import { SCHEMA_VERSION } from '../../../../constants'
 
 export class GetStatusHandler implements APIHandler {
   storageAdapter: StorageAdapter
+  walletRepository: WalletRepository
 
-  constructor (storageAdapter: StorageAdapter) {
+  constructor (storageAdapter: StorageAdapter, walletRepository: WalletRepository) {
     this.storageAdapter = storageAdapter
+    this.walletRepository = walletRepository
   }
 
   async handle (): Promise<GetStatusResponse> {
@@ -16,10 +19,11 @@ export class GetStatusHandler implements APIHandler {
     const currentWalletId = (await this.storageAdapter.get('currentWalletId')) as (string | null)
     const passwordPublicKey = (await this.storageAdapter.get('passwordPublicKey')) as (string | null)
     const schemaVersion = (await this.storageAdapter.get('schemaVersion')) as (number | null)
+    const hasAnyWallet = await this.walletRepository.hasAnyWallet()
 
     const ready = schemaVersion !== SCHEMA_VERSION
 
-    return { passwordSet: passwordPublicKey != null, network, currentWalletId, ready }
+    return { passwordSet: passwordPublicKey != null, network, currentWalletId, ready, hasAnyWallet }
   }
 
   validatePayload (payload: EmptyPayload): string | null {
