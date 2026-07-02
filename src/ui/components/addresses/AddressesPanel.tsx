70614531a5f17e2db30d81a3e3517a59e17792ad
AlexeyTripleA <andreewsky.alexey@gmail.com>
2026-07-03 01:04:22 +0300

    Update addresses list items styles

--- src/ui/components/addresses/AddressesPanel.tsx
+++ src/ui/components/addresses/AddressesPanel.tsx
@@ -216,7 +216,7 @@ export const AddressesPanel: React.FC<AddressesPanelProps> = ({ currentNetwork }
             onClick={() => { void handleCreate() }}
             disabled={isLoading || isGenerating}
           >
-            {isGenerating ? 'Creating...' : 'Create new address'}
+            {isGenerating ? 'Loading...' : 'Add one more address'}
           </Button>
           )}
     </div>
