cf02c88fb32444d9626e7a6315cff7bbde08c6d9
AlexeyTripleA <andreewsky.alexey@gmail.com>
2026-08-21 19:38:22 +0300

    replace topup sum from credits to dash on congratulations state

--- src/ui/states/topup/stages/Stage4Success.tsx
+++ src/ui/states/topup/stages/Stage4Success.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Text, ProgressStepBar, CreditsIcon } from 'dash-ui-kit/react'
+import { Button, Text, ProgressStepBar, DashLogo } from 'dash-ui-kit/react'
 import { TitleBlock } from '../../../components/layout/TitleBlock'
 import { creditsToDash } from '../../../../utils'
 
@@ -21,8 +21,8 @@ function formatDate (date: Date): string {
   return date.toLocaleDateString('en-GB').replace(/\//g, '.')
 }
 
-function formatCredits (credits: bigint): string {
-  return Number(credits).toLocaleString('en-US').replace(/,/g, ' ')
+function formatDash (credits: bigint): string {
+  return creditsToDash(credits).toLocaleString('en-US', { maximumFractionDigits: 8 })
 }
 
 export function Stage4Success ({
@@ -31,9 +31,8 @@ export function Stage4Success ({
   dashRate,
   onDone
 }: Stage4SuccessProps): React.JSX.Element {
-  const usdValue = result != null && dashRate != null
-    ? creditsToDash(result.topUpAmount) * dashRate
-    : null
+  const dashAmount = result != null ? creditsToDash(result.topUpAmount) : null
+  const usdValue = dashAmount != null && dashRate != null ? dashAmount * dashRate : null
 
   return (
     <div className='flex flex-col h-full'>
@@ -56,14 +55,14 @@ export function Stage4Success ({
           <div className='flex items-center justify-between'>
             <div className='flex items-center gap-2.5'>
               <div className='w-[30px] h-[30px] rounded-full bg-dash-primary-dark-blue/[0.05] flex items-center justify-center'>
-                <CreditsIcon size={14} className='text-dash-brand' />
+                <DashLogo size={14} className='text-dash-brand' />
               </div>
-              <Text size='sm' weight='medium'>Credits</Text>
+              <Text size='sm' weight='medium'>Dash</Text>
             </div>
             <div className='flex flex-col items-end gap-1'>
               <Text size='sm' weight='medium' className='tracking-[-0.03em]'>
-                <span className='font-extrabold text-dash-brand'>+ {formatCredits(result.topUpAmount)}</span>
-                {' '}Credits
+                <span className='font-extrabold text-dash-brand'>+ {formatDash(result.topUpAmount)}</span>
+                {' '}Dash
               </Text>
               {usdValue != null && (
                 <Text size='xs' className='text-dash-primary-dark-blue/35'>
