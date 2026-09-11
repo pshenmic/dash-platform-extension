b0a02f202964c3a98355e5dde29a1ccdc78dc8fc
AlexeyTripleA <andreewsky.alexey@gmail.com>
2026-09-11 22:09:48 +0300

    Add showgrid condition to PageWithHeader

--- src/ui/components/layout/PageWithHeader.tsx
+++ src/ui/components/layout/PageWithHeader.tsx
@@ -8,9 +8,18 @@ interface PageWithHeaderProps {
 }
 
 const PageWithHeader: FC<PageWithHeaderProps> = ({ children, showGrid = false }) => {
+  if (!showGrid) {
+    return (
+      <>
+        <Header />
+        {children}
+      </>
+    )
+  }
+
   return (
     <>
-      {showGrid && <DashboardGridBackground />}
+      <DashboardGridBackground />
       <Header />
       <div className='relative z-[1] min-w-0 w-full flex flex-col'>
         {children}
